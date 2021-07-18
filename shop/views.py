2e459a75fe47af387f4814e784253b40a93cfa8d
malek-laatiri <malek.laatiri73@gmail.com>
2021-07-18 12:28:20 +0100

    paginator

--- shop/views.py
+++ shop/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from requests import request
 
 from .models import Product
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
+
+    #Search code
     product_object = Product.objects.all()
     item_name=request.GET.get('item_name')
     if item_name!='' and item_name is not None:
         product_object=product_object.filter(title__icontains=item_name)
+
+    #Paginator code
+    paginator=Paginator(product_object,4)
+    page=request.GET.get('page')
+    product_object=paginator.get_page(page)
     return render(request, 'shop/index.html', {'product_object': product_object})
+
+
+def detail(request,id):
+    product=Product.objects.get(id=id)
+    return  render(request,'shop/detail.html',{'product_object':product})
